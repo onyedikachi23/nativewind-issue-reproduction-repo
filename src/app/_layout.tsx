6073aeeee6caeb113b96c010320329a31b5fd033
onyedkichi23 <ozohenrywk@gmail.com>
2024-11-05 12:27:50 +0100

    first commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,12 @@
+/** @format */
+
+import { Stack } from "expo-router";
+import "../../global.css";
+
+export default function RootLayout() {
+	return (
+		<Stack>
+			<Stack.Screen name="index" />
+		</Stack>
+	);
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+/** @format */
+
+import { Text, View } from "react-native";
+
+export default function Index() {
+	return (
+		<View className="flex flex-1 justify-center items-center">
+			<Text className="font-bold text-sm">AgricNexus</Text>
+		</View>
+	);
+}
